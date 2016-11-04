6110326d40d7f21e957b2869eb0b9a4f48f31727
Margie Nevarez <margie.ynm@gmail.com>
2016-11-04 10:51:35 -0700

    assign values to inputs selected

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,16 @@
 
 
 //user interface
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    debugger;
+    var size = parseInt($("#size").val());
+    var sauce = parseInt($("#sauce").val());
+    var extra = parseInt($("#extra").val());
+    var toppings = 0;
+    $("input:checkbox[name=toppings]:checked").each(function() {
+      toppings++;
+    });
+  });
+});
