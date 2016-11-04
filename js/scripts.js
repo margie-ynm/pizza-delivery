d4efc96acbab22e8dceef1e1a35a93f7b4b16773
Margie Nevarez <margie.ynm@gmail.com>
2016-11-04 15:02:53 -0700

    add styling to page

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 //business logic
-function Pizza(size, sauce, extra, toppings) {
+function Pizza(size, sauce, cheese, toppings) {
   debugger;
   this.sizeChoice = size;
   this.sauceChoice = sauce;
-  this.extraChoice = extra;
+  this.cheeseChoice = cheese;
   this.toppingsChoice = toppings;
 }
 
 Pizza.prototype.cost = function() {
   debugger;
-  var calculate = this.sizeChoice + this.sauceChoice + this.extraChoice + this.toppingsChoice;
+  var calculate = this.sizeChoice + this.sauceChoice + this.cheeseChoice + this.toppingsChoice;
   return calculate;
 }
 
@@ -21,12 +21,12 @@ $(document).ready(function() {
     $("#output").empty();
     var sizeChoice = parseInt($("#size").val());
     var sauceChoice = parseInt($("#sauce").val());
-    var extraChoice = parseInt($("#extra").val());
+    var cheeseChoice = parseInt($("#cheese").val());
     var toppingsChoice = 0;
     $("input:checkbox[name=toppings]:checked").each(function() {
       toppingsChoice++;
     });
-    var newPizza = new Pizza(sizeChoice, sauceChoice, extraChoice, toppingsChoice);
+    var newPizza = new Pizza(sizeChoice, sauceChoice, cheeseChoice, toppingsChoice);
 
     $("#output").append("<h3>" + "The total cost for your pizza is:" + " " + "$" + newPizza.cost() + "</h2>");
   });
